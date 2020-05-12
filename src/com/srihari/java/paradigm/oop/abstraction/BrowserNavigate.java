7be3b0f3668d0154263a1f81c3cd0c05deb51682
Srihari Singuru <srihari.singuru@outlook.com>
2020-05-12 22:44:47 +0530

    Object Oriented Programming - Abstraction

---
+++ src/com/srihari/java/paradigm/oop/abstraction/BrowserNavigate.java
@@ -0,0 +1,8 @@
+package com.srihari.java.paradigm.oop.abstraction;
+
+public class BrowserNavigate {
+    public static void main(String[] args) {
+        var browser = new Browser();
+        browser.navigate("www.google.com");
+    }
+}
