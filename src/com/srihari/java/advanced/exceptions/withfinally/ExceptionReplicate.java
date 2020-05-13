659041f81100d76a95dc963839a883aeb9cc451a
Srihari Singuru <srihari.singuru@outlook.com>
2020-05-13 22:42:04 +0530

    Exception Handling - with finally

---
+++ src/com/srihari/java/advanced/exceptions/withfinally/ExceptionReplicate.java
@@ -0,0 +1,34 @@
+package com.srihari.java.advanced.exceptions.withfinally;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * Looking bit ugly
+ */
+public class ExceptionReplicate {
+    private static FileReader reader = null;
+
+    public static void show() {
+        try {
+            reader = new FileReader("dummy.txt");
+            var value = reader.read();
+        }
+        catch (FileNotFoundException ex) {
+            System.out.println("File not found");
+        }
+        catch (IOException e) {
+            System.out.println("Couldn't read value");
+        }
+        finally {
+            if(reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
---
+++ src/com/srihari/java/advanced/exceptions/withfinally/ExceptionsApplication.java
@@ -0,0 +1,7 @@
+package com.srihari.java.advanced.exceptions.withfinally;
+
+public class ExceptionsApplication {
+    public static void main(String[] args) {
+        ExceptionReplicate.show();
+    }
+}
