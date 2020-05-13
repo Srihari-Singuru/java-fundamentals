3a4c75025e46ca6e17b25f139573ee68bbae9222
Srihari Singuru <srihari.singuru@outlook.com>
2020-05-13 16:41:12 +0530

    Interfaces - Tight coupling (need to refactor)

---
+++ src/com/srihari/java/interfaces/tightcouple/TaxReport.java
@@ -0,0 +1,18 @@
+package com.srihari.java.interfaces.tightcouple;
+
+public class TaxReport {
+
+    /* Tightly coupled with TaxCalculator
+       if constructor changes, all the dependents need to be changed
+       or if implementation changes, then all the dependents need to be re-compiled etc. */
+    private final TaxCalculator calculator;
+
+    public TaxReport() {
+        calculator = new TaxCalculator(1_00_000);
+    }
+
+    public void show(){
+        var tax = calculator.calculateTax();
+        System.out.println(tax);
+    }
+}
