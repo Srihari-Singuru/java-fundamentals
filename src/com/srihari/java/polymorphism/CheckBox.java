11be76ae34700fe77dd178d0df4990f26b8a474b
Srihari Singuru <srihari.singuru@outlook.com>
2020-05-13 16:01:39 +0530

    Object Oriented Programming - Polymorphism

---
+++ src/com/srihari/java/polymorphism/CheckBox.java
@@ -0,0 +1,9 @@
+package com.srihari.java.polymorphism;
+
+public class CheckBox extends UIControl {
+
+    @Override
+    public void render() {
+        System.out.println("Rendering CheckBox");
+    }
+}
