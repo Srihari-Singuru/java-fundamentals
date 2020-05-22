2dc345f0e884e6ada6e85a4a708456402fe9dda1
Srihari Singuru <srihari.singuru@outlook.com>
2020-05-22 16:20:47 +0530

    Collections - Iteration

---
+++ src/com/srihari/java/advanced/collections/iteration/CustomList.java
@@ -0,0 +1,41 @@
+package com.srihari.java.advanced.collections.iteration;
+
+import java.util.Iterator;
+
+public class CustomList<T> implements Iterable<T> {
+    private T[] items = (T[]) new Object[10];
+    private int count;
+
+    public void add(T item) {
+        items[count++] = item;
+    }
+
+    public T get(int index) {
+        return items[index];
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new CustomIterator(this);
+    }
+
+    // If "items" data structure or implementaton changes, then only this class changes
+    private class CustomIterator implements Iterator<T> {
+        private CustomList<T> list;
+        private int index;
+
+        public CustomIterator(CustomList<T> list){
+            this.list = list;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return (index < list.count);
+        }
+
+        @Override
+        public T next() {
+            return list.items[index++];
+        }
+    }
+}
