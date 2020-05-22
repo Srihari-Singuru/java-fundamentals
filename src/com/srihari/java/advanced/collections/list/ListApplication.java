0cee73516f50ccb7cd6d5cdbfab0847aa12328c0
Srihari Singuru <srihari.singuru@outlook.com>
2020-05-22 22:25:16 +0530

    Collections - List capabilities

---
+++ src/com/srihari/java/advanced/collections/list/ListApplication.java
@@ -0,0 +1,33 @@
+package com.srihari.java.advanced.collections.list;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ListApplication {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        // Add an item at a given index
+        list.add(0, "!");
+        Collections.addAll(list, "a", "b", "c");
+        System.out.println(list);
+
+        var first = list.get(0);
+        System.out.println(first);
+
+        list.set(0, "!!");  // Replaces the element at the index
+        first = list.get(0);
+        System.out.println(first);
+
+        list.remove(0);
+        System.out.println(list);
+
+        list.add("a");
+        var index = list.indexOf("a");  // first index
+        System.out.println(index);
+        var lastIndex = list.lastIndexOf("a");  // last index
+        System.out.println(lastIndex);
+
+        System.out.println(list.subList(0, 2)); // from included, to excluded; return new list hence existing list is not modified
+    }
+}
